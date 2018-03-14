6b61fd52965a7e957a60f3dbb40ef885daa4de1a
Kirapan <pxqpxqpxq@hotmail.com>
2018-03-14 14:49:36 +0000

    first version

---
+++ charposition.js
@@ -0,0 +1,13 @@
+function charPosition(input) {
+  var result = {};
+  input = input.split(' ').join('').split('');
+  for (var i = 0; i < input.length; i++) {
+    var keys = input[i];
+    if (result[keys] === undefined) {
+      result[keys] = [i];
+    } else {
+      result[keys].push(i);
+    }
+  } return result;
+}
+console.log(charPosition("lighthouse in the house"));
